6d7e8830e6c13ee0508d0c4feaee1fc99988a1ae
nandanjoshi17 <nnandanjoshi@gmail.com>
2026-06-05 12:02:06 +0000

    var and const in js

---
+++ basics/variables.js
@@ -0,0 +1,14 @@
+const acid = 1234
+let email = "abc@a.com"
+var pass = "1234"
+city ="Delhi"
+
+console.table([acid,email,pass,city])// table banse
+/* 
+prefer not to use var 
+bcz of isssue in block scope and function scope
+badhi jagya ae var use karisu to badhi jagya ae var ni value change thai jase
+let and const are block scope
+const is used for constant value which cannot be changed
+let and var are used for variable value which can be changed
+*/
