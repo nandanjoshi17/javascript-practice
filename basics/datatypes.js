1313eb5113edee28b9c2f4381dc86c355df71b9f
nandanjoshi17 <nnandanjoshi@gmail.com>
2026-06-06 11:27:44 +0000

    datatypes in js

---
+++ basics/datatypes.js
@@ -0,0 +1,14 @@
+" use strict" // treat all js code as newer version
+
+//alert(3+3) will show popup of 6 in browser 
+console.log(3 + 3)// op =6
+
+let name = "nandan"// string in ""
+let age = 18// int => 2 to 2 power 53 | bigint for big numbers
+let isonline = false//boolean true or false
+// null =>standalone value
+let state; // null used
+//symbol => unique
+console.log(typeof age); // op int
+console.log(typeof null); // op object
+console.log(typeof undefined);// op undefined
