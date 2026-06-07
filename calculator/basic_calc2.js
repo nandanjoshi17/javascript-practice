bc00c3cfee5b487e67b40c53dae50b01e5f82004
nandanjoshi17 <nnandanjoshi@gmail.com>
2026-06-07 13:51:36 +0000

    basic calc

---
+++ calculator/basic_calc2.js
@@ -0,0 +1,19 @@
+let num1 = Number(prompt("Enter 1st number : "));
+let num2 = Number(prompt("Enter 2nd number : "));
+
+console.log("Addition",num1 + num2);
+console.log("subtraction",num1 - num2);
+console.log("Multipliction",num1*num2);
+if(num2!=0)
+{
+    console.log("Division",num1 /num2);
+}
+else
+{
+    console.log("Error can not divide by 0");
+    
+}
+/* this code will work in browser only 
+o/p says prompt not defined */
+
+
