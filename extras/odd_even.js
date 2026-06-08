6c82e688cc9c701e078a7f2c8324e184a9e84a72
nandanjoshi17 <nnandanjoshi@gmail.com>
2026-06-08 16:44:20 +0000

    odd even numbers using for loop and while loop

---
+++ extras/odd_even.js
@@ -0,0 +1,25 @@
+console.log("Odd Numbers upto 10");
+for(let i=1;i<=10;i=i+2)
+{
+    console.log(i);
+}
+console.log("Even Numbers upto 10");
+for(let j=0;j<=10;j=j+2) // 0 is even number
+{
+    console.log(j);
+}
+console.log("odd number using while loop");
+let k=1
+while(k<=10)
+{
+    console.log(k);
+    k=k+2;
+}
+console.log("Even number using while loop");
+let l=0;
+while(l<=10)
+{
+    console.log(l);
+    l=l+2;
+    
+}
